e64053e2c0ca5a3f87d256bc701d13a17e96dc1f
Wanga Kanjala <wangakanjala@gmail.com>
2026-01-01 16:29:06 +0200

    added select user data file

---
+++ www/js/select-user-data.js
@@ -0,0 +1,11 @@
+function fetchUsers() {
+    db.executeSql('SELECT * FROM users', [], function(rs) {
+        // rs.rows.length tells you how many records were found
+        for (var i = 0; i < rs.rows.length; i++) {
+            var user = rs.rows.item(i);
+            console.log('User found: ' + user.name + ' (' + user.email + ')');
+        }
+    }, function(error) {
+        console.error('SELECT error: ' + error.message);
+    });
+}
