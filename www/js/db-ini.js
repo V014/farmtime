296d93637000ec49025e256a258f1e70375e033f
Wanga Kanjala <wangakanjala@gmail.com>
2026-01-01 16:41:51 +0200

    addded comments for project

--- www/js/db-ini.js
+++ www/js/db-ini.js
@@ -1,13 +1,16 @@
 var db = null;
 
 document.addEventListener('deviceready', function() {
-    // Open (or create) the database
+    // This line creates the physical .db file if it's the first time the app is opened.
+    // Otherwise, it simply establishes a connection to the existing file.
+    // Open the database
     db = window.sqlitePlugin.openDatabase({
         name: 'farmtime.db',
         location: 'default', // Essential for Android/iOS
         androidDatabaseProvider: 'system' // Recommended for modern Android
     });
 
+    // If the tables already exist, does nothing, and moves to the "Success" callback
     // Initialize your tables
     db.transaction(function(tx) {
         tx.executeSql('CREATE TABLE IF NOT EXISTS settings (key text primary key, value text)');
