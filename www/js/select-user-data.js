ae2925804329c94ed6718882bdd83a9842ac00ca
Wanga Kanjala <wangakanjala@gmail.com>
2026-01-13 22:01:57 +0200

    updated user query

--- www/js/select-user-data.js
+++ www/js/select-user-data.js
@@ -3,7 +3,7 @@ function fetchUsers() {
         // rs.rows.length tells you how many records were found
         for (var i = 0; i < rs.rows.length; i++) {
             var user = rs.rows.item(i);
-            console.log('User found: ' + user.name + ' (' + user.email + ')');
+            console.log('User found: ' + user.username + ' (' + user.role + ')');
         }
     }, function(error) {
         console.error('SELECT error: ' + error.message);
