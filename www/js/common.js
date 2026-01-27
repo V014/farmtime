06e18dc61a78e6968e3351508c8695b5560fb0b6
Wanga Kanjala <wangakanjala@gmail.com>
2026-01-27 19:33:24 +0200

    adapting ui for adding crop

--- www/js/common.js
+++ www/js/common.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
                 </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="layout-static.html">Add crops</a>
+                        <a class="nav-link" href="add_crop.html">Add crops</a>
                         <a class="nav-link" href="layout-sidenav-light.html">List crops</a>
                     </nav>
                 </div>
