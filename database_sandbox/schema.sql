fa2ed3489c8f3b283fcf82ec9911df71cd3a1feb
Wanga Kanjala <wangakanjala@gmail.com>
2026-01-29 21:55:07 +0200

    updated class diagram and schema

--- database_sandbox/schema.sql
+++ database_sandbox/schema.sql
@@ -1,65 +1,66 @@
 -- 1. User Management
 CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
-    role TEXT, -- enum handled as text
-    date_registered DATETIME DEFAULT,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP
+    role TEXT NOT NULL, -- enum handled as text
+    date_registered DATETIME NOT NULL,
+    date_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE user_profile (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    biography TEXT,
-    contact INTEGER,
-    address TEXT,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+    biography TEXT NOT NULL,
+    contact INTEGER NOT NULL,
+    address TEXT NOT NULL,
+    date_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 -- 2. Land Organization
 CREATE TABLE Field (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     field_name TEXT NOT NULL,
-    area_size INTEGER,
-    location TEXT,
-    area_unit TEXT, -- e.g., 'Hectares', 'Acres'
-    soil_type TEXT, -- enum handled as text
-    tenure_type TEXT, -- e.g., 'Customary', 'Leasehold'
-    date_recorded DATETIME DEFAULT,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+    area_size INTEGER NOT NULL,
+    location TEXT NOT NULL,
+    area_unit TEXT NOT NULL, -- e.g., 'Hectares', 'Acres'
+    soil_type TEXT NOT NULL, -- enum handled as text
+    tenure_type TEXT NOT NULL, -- e.g., 'Customary', 'Leasehold'
+    date_recorded DATETIME NOT NULL,
+    date_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE plot (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     field_id INTEGER NOT NULL,
-    plot_name TEXT,
-    area_size INTEGER,
-    area_unit TEXT,
-    date_recorded DATETIME DEFAULT,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+    plot_name TEXT NOT NULL,
+    area_size INTEGER NOT NULL,
+    area_unit TEXT NOT NULL, -- e.g., 'Square meters', 'Acres'
+    date_recorded DATETIME NOT NULL,
+    date_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (field_id) REFERENCES Field(id) ON DELETE CASCADE
 );
 
 -- 3. Crops and Varieties
 CREATE TABLE crop_variety (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     crop_name TEXT NOT NULL,
-    crop_variety TEXT,
-    availability TEXT, -- enum
-    date_recorded DATETIME DEFAULT CURRENT_TIMESTAMP,
+    crop_variety TEXT NOT NULL,
+    availability TEXT NOT NULL, -- enum
     pH_requirement INTEGER,
     temperature_celsius INTEGER,
     rainfall_mm INTEGER,
-    season_requirement TEXT,
+    season_requirement TEXT NOT NULL, -- enum
     maturity_rate INTEGER, -- Days to harvest
     yield_estimate INTEGER,
-    yield_unit TEXT,
-    treatment TEXT,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP
+    yield_unit TEXT, -- e.g., 'kg per hectare'
+    common_pests TEXT,
+    treatment TEXT NOT NULL,
+    date_recorded DATETIME NOT NULL,
+    date_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE supplier (
