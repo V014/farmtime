aa744709979146676fe25296c5699f6d56291d0e
Wanga Kanjala <wangakanjala@gmail.com>
2026-02-04 15:05:48 +0200

    adjusting navbar

--- www/js/common.js
+++ www/js/common.js
@@ -15,9 +15,8 @@ $(document).ready(function() {
                 </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="add_crop.html">Add crops</a>
-                        <a class="nav-link" href="add_crop_variety.html">Crop variety</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">List crops</a>
+                        <a class="nav-link" href="list_crops.html">List crops</a>
+                        <a class="nav-link" href="Crop_varieties.html">Crop variety</a>
                     </nav>
                 </div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
