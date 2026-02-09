01604973335e46c05111bbeca8707c04f6e473e5
Wanga Kanjala <wangakanjala@gmail.com>
2026-02-09 11:26:00 +0200

    fixing authentication

--- www/js/auth-reg.js
+++ www/js/auth-reg.js
@@ -18,7 +18,7 @@ function handleLogin() {
     const pass = document.getElementById('password').value;
 
     db.transaction(function(tx) {
-        tx.executeSql('SELECT * FROM users WHERE username=? AND password=?', [user, pass], function(tx, results) {
+        tx.executeSql('SELECT * FROM user WHERE username=? AND password=?', [user, pass], function(tx, results) {
             if (results.rows.length > 0) {
                 // alert("Login Successful!");
                 window.location.href = "../farmer/index.html"; // Move to the next page
@@ -33,13 +33,13 @@ function handleSignup() {
     const user = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
     const passConfirm = document.getElementById('passwordConfirm').value;
-    const contact = parseInt(document.getElementById('contact_number').value);
+    const contact = parseInt(document.getElementById('contact').value);
     const address = document.getElementById('address').value;
     const now = new Date().toISOString();
 
-    if (pass == passConfirm) {
+    if (pass === passConfirm) {
         db.transaction(function(tx) {
-        tx.executeSql('INSERT INTO users (username, password, role, date_registered) VALUES (?, ?, ?, ?)', [user, pass, 'farmer', now], function(tx, res) {
+        tx.executeSql('INSERT INTO user (username, password, role, date_registered) VALUES (?, ?, ?, ?)', [user, pass, 'farmer', now], function(tx, res) {
             const userId = res.insertId;
             tx.executeSql('INSERT INTO user_profile (user_id, contact, address, date_updated) VALUES (?, ?, ?, ?)', [userId, contact, address, now], function(tx, res2) {
                 // alert("Signup successful! User ID: " + userId);
