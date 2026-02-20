5883af964187473df75e51efa0791be0f350bdeb
Wanga Kanjala <wangakanjala@gmail.com>
2026-02-20 15:08:54 +0200

    added delete crop variety feature

---
+++ www/js/delete_crop_variety.js
@@ -0,0 +1,35 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Get crop_id from URL query parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const cropId = urlParams.get('crop_id');
+
+    if (cropId) {
+        // Delete the crop variety from the database
+        db.transaction(function(tx) {
+            tx.executeSql('DELETE FROM crop_variety WHERE id = ?', [cropId], function(tx, res) {
+                alert("Crop variety deleted successfully!");
+                window.location.href = "../farmer/crop_variety.html";
+            }, function(tx, err) {
+                alert("Error deleting crop variety: " + err.message);
+            });
+        });
+    } else {
+        alert('No crop variety selected');
+        window.location.href = '../farmer/crop_variety.html';
+    }
+};
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing 
+ */
--- www/js/list_varieties.js
+++ www/js/list_varieties.js
@@ -4,7 +4,6 @@ function listVarieties() {
     db.transaction(function(tx) {
         tx.executeSql('SELECT * FROM crop_variety', [], function(tx, res) {
             const tbody = document.getElementById('crop_varieties_table').getElementsByTagName('tbody')[0];
-            // tbody.innerHTML = '';   // clear sample rows
             for (let i = 0; i < res.rows.length; i++) {
                 const cv = res.rows.item(i);
                 const tr = document.createElement('tr');
@@ -13,9 +12,8 @@ function listVarieties() {
                     <td>${cv.crop_name}</td>
                     <td>${cv.crop_variety}</td>
                     <td>${cv.availability}</td>
-                    <td>${cv.pH_min ?? ''}-${cv.pH_max ?? ''}</td>
-                    <td>${cv.temp_min ?? ''}-${cv.temp_max ?? ''}</td>
                     <td>${cv.yield_estimate ?? ''}${cv.yield_unit ? ' ' + cv.yield_unit : ''}</td>
+                    <td><button class="btn btn-danger" onclick="deleteVariety(${cv.id})">Delete</button></td>
                 `;
                 tr.addEventListener('click', () => {
                     window.location.href = `../farmer/edit_crop_variety.html?crop_id=${cv.id}`;
